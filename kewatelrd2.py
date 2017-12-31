af7ad909f7594fd1af9921916724e7b4fea67344
elnemr159 <34989011+elnemr159@users.noreply.github.com>
2017-12-31 23:25:35 +0200

    Hack

---
+++ kewatelrd2.py
@@ -0,0 +1,20 @@
+UW PICO 5.09         File: kewatelrad2          Modified
+
+try:
+      smtpserver-login["user.passw")
+
+
+     print("[+] password found ====>   ")
+
+     break:
+
+
+   except smtplib.snIpAthenticationError
+
+
+   print("{+} password incorect ====> %s")
+
+
+
+^G Get Hel^O WriteOu^R Read Fi^Y Prev Pg^K Cut Tex^C Cur Pos
+^X Exit   ^J Justify^W Where i^V Next Pg^U UnCut T^T To Spe
